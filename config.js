108d40a893338dce83c63ceb407ee7e8db87f136
thihxm <thiagohxm@hotmail.com>
2017-03-19 00:08:21 -0300

    Change db again
    updated

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = function(App) {
 
   return {
 
-    db_path: 'mongodb://csgo-shard-00-00-5a00a.mongodb.net/csgo',
+    db_path: 'mongodb://admin:admin2308@csgo-shard-00-00-5a00a.mongodb.net:27017,csgo-shard-00-01-5a00a.mongodb.net:27017,csgo-shard-00-02-5a00a.mongodb.net:27017/csgo?ssl=true&replicaSet=CSGO-shard-0&authSource=admin',
 
     db_collections: ['users', 'history', 'items'],
 
